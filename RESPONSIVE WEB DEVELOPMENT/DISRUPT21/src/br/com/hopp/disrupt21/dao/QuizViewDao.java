3699619859f3a24841b12086557c295579710583
dss266 <izi.davi2017@gmail.com>
2020-11-19 21:09:38 -0300

    Adição do metodo pesquisar por Id

--- RESPONSIVE WEB DEVELOPMENT/DISRUPT21/src/br/com/hopp/disrupt21/dao/QuizViewDao.java
+++ RESPONSIVE WEB DEVELOPMENT/DISRUPT21/src/br/com/hopp/disrupt21/dao/QuizViewDao.java
@@ -3,9 +3,13 @@ package br.com.hopp.disrupt21.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import br.com.hopp.disrupt21.to.QuizTo;
 import br.com.hopp.disrupt21.to.QuizViewTo;
 
 
 public interface QuizViewDao {
+	
 	List<QuizViewTo> lista() throws SQLException, Exception;
+	
+	QuizViewTo pesquisar(int id)throws SQLException, Exception;
 }
